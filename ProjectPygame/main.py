864a797f40d02add7314f05ce9420a058ca5b57f
jezcall <rijic03@mail.ru>
2020-01-03 00:37:22 +0300

    Можно строить башни

--- ProjectPygame/main.py
+++ ProjectPygame/main.py
@@ -1,22 +1,26 @@
 import os
 import math
 
-import time
 import pygame
 
 # from mob_1 import first_mob
 
-path = [[-50, 455], [154, 455], [200, 495], [710, 507], [767, 424], [710, 298], [450, 298], [390, 205], [450, 110],
-         [1250, 110]]
-# path = [[-50, 455], [157, 455], [228, 512], [268, 512], [343, 480], [390, 512], [689, 515], [744, 481], [767, 423], [741, 356], [696, 340], [494, 340], [437, 247], [413, 210], [430, 153], [456, 117], [475, 98], [1250, 78]]
+path = [[-50, 455], [154, 455], [200, 495], [710, 507], [767, 424], [710, 298], [450, 298], [390, 205],
+        [450, 110], [1250, 110]]
+towers_first_lvl = [[]]
 pygame.init()
 all_sprites = pygame.sprite.Group()
 mob_sprites = pygame.sprite.Group()
+TOWER_SPRITES = pygame.sprite.Group()
+TOWER_BUILDING_SPRITES = pygame.sprite.Group()
+TOWER_BOUGHT = pygame.sprite.Group()
+TOWER_BOUGHT_RING = pygame.sprite.Group()
+BUILDING_PLACE_COORDS = [[400, 385], [580, 380], [495, 595], [850, 380], [497, 160], [685, 157], [870, 155]]
 
 
 def load_image(name):
     fullname = os.path.join('img', name)
-    image = pygame.image.load(fullname).convert_alpha()
+    image = pygame.image.load(fullname)
     return image
 
 
@@ -34,6 +38,7 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.y = path[0][1]
         self.speed_increase = 10
         self.rect = self.rect.move(x, y)
+        self.flipped = False
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -41,8 +46,6 @@ class AnimatedSprite(pygame.sprite.Sprite):
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
-                # r = sheet.subsurface(pygame.Rect(frame_location, self.rect.size))
-                # r = pygame.transform.scale(self.image, (150, 200))
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
@@ -62,10 +65,14 @@ class AnimatedSprite(pygame.sprite.Sprite):
 
         length = math.sqrt((dirn[0]) ** 2 + (dirn[1]) ** 2)
         dirn = (dirn[0] / length * self.speed_increase, dirn[1] / length * self.speed_increase)
-        # if dirn[0] < 0 and not(self.flipped):
-        #     self.flipped = True
-        #     for x, img in enumerate(self.imgs):
-        #         self.imgs[x] = pygame.transform.flip(img, True, False)
+        if (dirn[0] < 0 and not (self.flipped)):
+            self.flipped = True
+            for x, img in enumerate(self.frames):
+                self.frames[x] = pygame.transform.flip(img, True, False)
+        elif dirn[0] > 0 and self.flipped:
+            self.flipped = False
+            for x, img in enumerate(self.frames):
+                self.frames[x] = pygame.transform.flip(img, True, False)
 
         move_x, move_y = ((self.x + dirn[0]), (self.y + dirn[1]))
         self.x = move_x
@@ -87,8 +94,29 @@ class AnimatedSprite(pygame.sprite.Sprite):
                     self.count += 1
         self.rect.x = self.x
         self.rect.y = self.y
-        print(self.count)
 
+
+class ArcherTower(pygame.sprite.Sprite):
+    image = load_image("b_p.png")
+    pass
+
+
+class BuildingPlaces(pygame.sprite.Sprite):
+    image = load_image("b_p.png")
+
+    def __init__(self, x, y):
+        super().__init__(TOWER_BUILDING_SPRITES)
+        self.image = BuildingPlaces.image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.building_place_num = BUILDING_PLACE_COORDS.index([x, y])
+
+
+for i in range(len(BUILDING_PLACE_COORDS)):
+    x = BUILDING_PLACE_COORDS[i][0]
+    y = BUILDING_PLACE_COORDS[i][1]
+    BuildingPlaces(x, y)
 
 clock = pygame.time.Clock()
 size = width, height = 1200, 705
@@ -100,9 +128,10 @@ sprite.rect = sprite.image.get_rect()
 all_sprites.add(sprite)
 screen.fill(pygame.Color('blue'))
 AnimatedSprite(load_image("walk-1.png"), 4, 1, -50, 255)
+
 fps = 10
 running = True
-drawing = True
+tower_building = False
 count = 0
 while running:
     for event in pygame.event.get():
@@ -110,23 +139,51 @@ while running:
             running = False
         if event.type == pygame.MOUSEMOTION:
             print(event.pos)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos[0], event.pos[1]
+            for sp in TOWER_BOUGHT:
+                if sp.rect.collidepoint(x, y):
+                    for s in TOWER_BUILDING_SPRITES:
+                        if s.rect.x == sp.rect.x and s.rect.y == sp.rect.y + 15:
+                            s.kill()
+                    TOWER_BOUGHT_RING = pygame.sprite.Group()
+                    TOWER_BOUGHT = pygame.sprite.Group()
+                else:
+                    TOWER_BOUGHT_RING = pygame.sprite.Group()
+                    TOWER_BOUGHT = pygame.sprite.Group()
+
+            for sp in TOWER_BUILDING_SPRITES:
+                if sp.rect.collidepoint(x, y):
+                    tower_building = sp.building_place_num
+                    TOWER_BOUGHT_RING = pygame.sprite.Group()
+                    TOWER_BOUGHT = pygame.sprite.Group()
+
+                    buy_tower_ring = pygame.sprite.Sprite()
+                    buy_tower_ring.image = pygame.transform.scale(load_image("tower-builder.png"), (100, 100))
+
+                    buy_tower_ring.rect = buy_tower_ring.image.get_rect()
+                    buy_tower_ring.rect.x = sp.rect.x
+                    buy_tower_ring.rect.y = sp.rect.y - 15
+                    TOWER_BOUGHT_RING.add(buy_tower_ring)
+
+                    buy_tower = pygame.sprite.Sprite()
+                    buy_tower.image = pygame.transform.scale(load_image("archer_tower_buy.png"), (40, 40))
+                    buy_tower.rect = buy_tower.image.get_rect()
+                    buy_tower.rect.x = sp.rect.x
+                    buy_tower.rect.y = sp.rect.y - 15
+                    TOWER_BOUGHT.add(buy_tower)
+
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
     mob_sprites.draw(screen)
-    # count += 1
-    # if count == 2:
-    #     first_mob.update()
-    #     count = 0
+    TOWER_BUILDING_SPRITES.draw(screen)
+    TOWER_BOUGHT_RING.draw(screen)
+    TOWER_BOUGHT.draw(screen)
     for m in mob_sprites:
         m.move()
         m.update()
-    count += 1
-    if count == 20:
-        count = 0
-    for i in range(1, len(path)):
-        pygame.draw.line(screen, (255, 0, 0), (path[i - 1]), (path[i]), 1)
-
-    # first_mob.move()
+    # for i in range(1, len(path)):
+    #     pygame.draw.line(screen, (255, 0, 0), (path[i - 1]), (path[i]), 1)
     pygame.display.flip()
     clock.tick(fps)
 pygame.quit()
