e062a48605b98a6c102ebee0d976b9d7f97fb158
jezcall <rijic03@mail.ru>
2020-01-05 00:09:39 +0300

    Почти все работет

--- ProjectPygame/main.py
+++ ProjectPygame/main.py
@@ -15,6 +15,7 @@ TOWER_SPRITES = pygame.sprite.Group()
 TOWER_BUILDING_SPRITES = pygame.sprite.Group()
 TOWER_BOUGHT = pygame.sprite.Group()
 TOWER_BOUGHT_RING = pygame.sprite.Group()
+ARCHERS = pygame.sprite.Group()
 BUILDING_PLACE_COORDS = [[400, 385], [580, 380], [495, 595], [850, 380], [497, 160], [685, 157], [870, 155]]
 
 
@@ -28,17 +29,117 @@ class AnimatedSprite(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y):
         super().__init__(mob_sprites)
         self.frames = []
+        self.frames_kill = []
+        self.count = 0
+        self.cut_sheet(sheet, columns, rows)
+        self.cut_sheet(load_image("kill_enemy.png"), 3, 1)
+        for i in range(len(self.frames)):
+            self.frames[i] = pygame.transform.scale(self.frames[i], (40, 40))
+        for i in range(len(self.frames_kill)):
+            self.frames_kill[i] = pygame.transform.scale(self.frames_kill[i], (60, 40))
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.x = path[0][0]
+        self.y = path[0][1]
+        self.kill_enemy = 0
+        self.speed_increase = 10
+        self.rect = self.rect.move(x, y)
+        self.flipped = False
+        self.health = 10
+        self.cur_frame_kill = 0
+        self.move_enemy = True
+
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                if columns == 3:
+                    self.frames_kill.append(sheet.subsurface(pygame.Rect(
+                        frame_location, self.rect.size)))
+                else:
+                    self.frames.append(sheet.subsurface(pygame.Rect(
+                        frame_location, self.rect.size)))
+
+    def update(self):
+        print(len(self.frames_kill))
+        if self.kill_enemy == 3:
+            self.kill()
+        if self.health <= 0:
+            self.kill_enemy += 1
+            self.cur_frame_kill = (self.cur_frame_kill + 1) % len(self.frames_kill)
+            self.image = self.frames_kill[self.cur_frame_kill]
+            self.move_enemy = False
+        else:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+        if self.x >= 1270:
+            self.kill()
+
+    def move(self):
+        if self.move_enemy:
+            x1, y1 = path[self.count]
+            if self.count + 1 >= len(path):
+                x2, y2 = (1250, 278)
+            else:
+                x2, y2 = path[self.count + 1]
+
+            dirn = ((x2 - x1) * 2, (y2 - y1) * 2)
+
+            length = math.sqrt((dirn[0]) ** 2 + (dirn[1]) ** 2)
+            dirn = (dirn[0] / length * self.speed_increase, dirn[1] / length * self.speed_increase)
+            if (dirn[0] < 0 and not (self.flipped)):
+                self.flipped = True
+                for x, img in enumerate(self.frames):
+                    self.frames[x] = pygame.transform.flip(img, True, False)
+            elif dirn[0] > 0 and self.flipped:
+                self.flipped = False
+                for x, img in enumerate(self.frames):
+                    self.frames[x] = pygame.transform.flip(img, True, False)
+
+            move_x, move_y = ((self.x + dirn[0]), (self.y + dirn[1]))
+            self.x = move_x
+            self.y = move_y
+            # Go to next point
+            if dirn[0] >= 0:  # moving right
+                if dirn[1] >= 0:  # moving down
+                    if self.x >= x2 and self.y >= y2:
+                        self.count += 1
+                else:
+                    if self.x >= x2 and self.y <= y2:
+                        self.count += 1
+            else:  # moving left
+                if dirn[1] > 0:  # moving down
+                    if self.x <= x2 and self.y >= y2:
+                        self.count += 1
+                else:
+                    if self.x <= x2 and self.y <= y2:
+                        self.count += 1
+            self.rect.x = self.x
+            self.rect.y = self.y
+
+
+class ArcherTower(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(ARCHERS)
+        self.frames = []
         self.count = 0
         self.cut_sheet(sheet, columns, rows)
         for i in range(len(self.frames)):
             self.frames[i] = pygame.transform.scale(self.frames[i], (40, 40))
         self.cur_frame = 0
+        self.x = x + 20
+        self.y = y + 70
         self.image = self.frames[self.cur_frame]
-        self.x = path[0][0]
-        self.y = path[0][1]
-        self.speed_increase = 10
         self.rect = self.rect.move(x, y)
         self.flipped = False
+        self.flipped_x = 0
+        self.radius = 170
+        # self.image = pygame.Surface((2 * self.radius, 2 * self.radius), pygame.SRCALPHA, 32)
+        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 10)
+        # self.rect = pygame.Rect(x - self.radius + 25, y - self.radius / 2 - 15, 2 * self.radius, 2 * self.radius)
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -48,57 +149,27 @@ class AnimatedSprite(pygame.sprite.Sprite):
                 frame_location = (self.rect.w * i, self.rect.h * j)
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
-
+    def attack(self):
+        for i in mob_sprites:
+            if math.sqrt((self.x - i.rect.x) ** 2 + (self.y - i.rect.y) ** 2 ) < self.radius:
+                i.health -= 1
+                self.flipped_x = self.x - i.rect.x
+                return True
+        return False
     def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
-
-    def move(self):
-
-        x1, y1 = path[self.count]
-        if self.count + 1 >= len(path):
-            x2, y2 = (1250, 278)
-        else:
-            x2, y2 = path[self.count + 1]
-
-        dirn = ((x2 - x1) * 2, (y2 - y1) * 2)
-
-        length = math.sqrt((dirn[0]) ** 2 + (dirn[1]) ** 2)
-        dirn = (dirn[0] / length * self.speed_increase, dirn[1] / length * self.speed_increase)
-        if (dirn[0] < 0 and not (self.flipped)):
-            self.flipped = True
-            for x, img in enumerate(self.frames):
-                self.frames[x] = pygame.transform.flip(img, True, False)
-        elif dirn[0] > 0 and self.flipped:
+        if self.attack():
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+        if self.flipped_x < 0 and self.flipped:
             self.flipped = False
             for x, img in enumerate(self.frames):
                 self.frames[x] = pygame.transform.flip(img, True, False)
-
-        move_x, move_y = ((self.x + dirn[0]), (self.y + dirn[1]))
-        self.x = move_x
-        self.y = move_y
-        # Go to next point
-        if dirn[0] >= 0:  # moving right
-            if dirn[1] >= 0:  # moving down
-                if self.x >= x2 and self.y >= y2:
-                    self.count += 1
-            else:
-                if self.x >= x2 and self.y <= y2:
-                    self.count += 1
-        else:  # moving left
-            if dirn[1] > 0:  # moving down
-                if self.x <= x2 and self.y >= y2:
-                    self.count += 1
-            else:
-                if self.x <= x2 and self.y <= y2:
-                    self.count += 1
-        self.rect.x = self.x
-        self.rect.y = self.y
-
-
-class ArcherTower(pygame.sprite.Sprite):
-    image = load_image("b_p.png")
-    pass
+        elif self.flipped_x > 0 and not self.flipped:
+            self.flipped = True
+            for x, img in enumerate(self.frames):
+                self.frames[x] = pygame.transform.flip(img, True, False)
+    def get(self):
+        return (self.x, self.y)
 
 
 class BuildingPlaces(pygame.sprite.Sprite):
@@ -111,6 +182,8 @@ class BuildingPlaces(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.building_place_num = BUILDING_PLACE_COORDS.index([x, y])
+def cause_enemy():
+    AnimatedSprite(load_image("walk-1.png"), 4, 1, -50, 255)
 
 
 for i in range(len(BUILDING_PLACE_COORDS)):
@@ -133,8 +206,13 @@ fps = 10
 running = True
 tower_building = False
 count = 0
+c = 0
+SPAWNENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(SPAWNENEMY, 1000)
 while running:
     for event in pygame.event.get():
+        if event.type == SPAWNENEMY:
+            AnimatedSprite(load_image("walk-1.png"), 4, 1, -50, 255)
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEMOTION:
@@ -148,6 +226,13 @@ while running:
                             s.kill()
                     TOWER_BOUGHT_RING = pygame.sprite.Group()
                     TOWER_BOUGHT = pygame.sprite.Group()
+                    tower = pygame.sprite.Sprite()
+                    tower.image = pygame.transform.scale(load_image("archer_tower.png"), (110, 110))
+                    tower.rect = tower.image.get_rect()
+                    tower.rect.x = sp.rect.x - 5
+                    tower.rect.y = sp.rect.y - 30
+                    TOWER_SPRITES.add(tower)
+                    ArcherTower(load_image("archer_2.png"), 6, 1, sp.rect.x + 25, sp.rect.y - 35)
                 else:
                     TOWER_BOUGHT_RING = pygame.sprite.Group()
                     TOWER_BOUGHT = pygame.sprite.Group()
@@ -173,15 +258,20 @@ while running:
                     buy_tower.rect.y = sp.rect.y - 15
                     TOWER_BOUGHT.add(buy_tower)
 
-    screen.fill((0, 0, 0))
     all_sprites.draw(screen)
     mob_sprites.draw(screen)
     TOWER_BUILDING_SPRITES.draw(screen)
     TOWER_BOUGHT_RING.draw(screen)
     TOWER_BOUGHT.draw(screen)
+    TOWER_SPRITES.draw(screen)
+    ARCHERS.draw(screen)
     for m in mob_sprites:
         m.move()
         m.update()
+    for a in ARCHERS:
+        a.update()
+        pygame.draw.circle(screen, (255, 0, 0, 50), a.get(), 170, 1)
+
     # for i in range(1, len(path)):
     #     pygame.draw.line(screen, (255, 0, 0), (path[i - 1]), (path[i]), 1)
     pygame.display.flip()
